c0e41725795556758af092c13f48c6920db7b4ad
nikhilsinghrathore1 <111992033+nikhilsinghrathore1@users.noreply.github.com>
2025-02-22 10:38:21 +0530

    dep_fixed_maybe

--- app/admindashboard/calender/page.tsx
+++ app/admindashboard/calender/page.tsx
@@ -1,17 +1,16 @@
-
-import React from 'react'
-import SideNavBar from '../components/SideNavBar'
-import { TopNavigation } from '../components/TopNavigation'
+import React from "react";
+import SideNavBar from "../components/SideNavBar";
+import { TopNavigation } from "../components/TopNavigation";
 
 const page = () => {
   return (
-    <div className='w-full h-screen flex'>
-               <SideNavBar current={"calender"}/>
-               <div className='w-[82%] h-full'>
-                              <TopNavigation/>
-               </div>
+    <div className="w-full h-screen flex">
+      <SideNavBar current={"calender"} />
+      <div className="w-[82%] h-full">
+        <TopNavigation />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
